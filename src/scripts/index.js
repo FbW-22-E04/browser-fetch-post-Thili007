b05266d69c22fd0f28b47a1267a2546f51615c90
Thili007 <tpunchihewage@gmail.com>
2022-10-05 04:28:36 +0200

    added files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,45 @@
-// The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
-import "babel-polyfill";
+"use strict";
 
+// The following line makes sure your styles are included in the project. Don't remove this.
+import "../styles/main.scss";
+import "babel-polyfill";
 
 // \/ All of your javascript should go here \/
 
+// const form = document.querySelector("form");
+
+const form = document.getElementById("submit-form");
+const formInput = document.querySelectorAll("input");
+
+form.addEventListener("submit", submitData);
+//get data
+async function submitData() {
+  const inputs = [];
+  formInput.forEach((input) => {
+    inputs.push(`${input.id}: ${input.value}`);
+  });
+  const jsonData = JSON.stringify(inputs);
+
+  const settings = {
+    method: "POST",
+    body: jsonData,
+  };
+
+  //send data
+  const fetchResponse = await fetch(
+    `https://my-json-server.typicode.com/typicode/demo/posts`,
+    settings
+  );
+  const data = await fetchResponse.json();
+  console.log(jsonData);
+  console.log(data);
+  return data;
+}
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  submitData().then((data) =>
+    alert(`Thank you for submitting your details. 
+User ID: ${data.id}`)
+  );
+});
